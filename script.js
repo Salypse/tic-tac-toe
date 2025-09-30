a1ee60e5e0b462b0af443802945388753797e172
Salypse <84153483+Salypse@users.noreply.github.com>
2025-09-30 13:55:27 -0500

    Update player turn text to current player

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function gameDisplay() {
+function newGameDisplay() {
     const gridContainer = document.querySelector("#game-container")
+    const currentPlayerText = document.querySelector(".current-player")
 
-    let currentPlayer = true
+    /* Player 1 is 0, Player2 is 1*/
+    let currentPlayer = Math.round(Math.random(1))
+
+    const player1 = newPlayer()
+    const player2 = newPlayer()
+    currentPlayerText.textContent = setDefaultText()
 
     let newGrid = function() {
         for (let i = 1; i <= 9; i++) {
@@ -19,19 +25,41 @@ function gameDisplay() {
     }
 
     function addSelection(item) {
-        if (currentPlayer && !item.textContent) {
+        if (currentPlayer === 0 && !item.textContent) {
             item.textContent = "X"
             item.style.color = "#2a9d8f";
-            currentPlayer = false
+            /*Switch to player2 turn */
+            currentPlayerText.textContent = `${player2.name}'s Turn`
+            currentPlayer = 1;
         }
-        else if (!currentPlayer && !item.textContent) {
+        else if (currentPlayer === 1 && !item.textContent) {
             item.textContent = "O"
             item.style.color = "#ecd717";
-            currentPlayer = true;
+            /*Switch to player1 turn */
+            currentPlayerText.textContent = `${player1.name}'s Turn`
+            currentPlayer = 0;
+        }
+    }
+
+    function setDefaultText() {
+        if (currentPlayer === 0) {
+            return `${player1.name}'s Turn`
+        } else if (currentPlayer === 1) {
+            return `${player2.name}'s Turn`
         }
     }
 
     return {newGrid}
 }
 
-let test = gameDisplay().newGrid()
+function gameController() {
+    let currentGame = newGameDisplay().newGrid()
+}
+
+function newPlayer() {
+    let name = prompt("Player Name: ");
+    return{name}
+}
+
+
+gameController()
