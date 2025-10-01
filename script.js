40ebf70945555d9e58d28746c244bcd1b7efc9f4
Salypse <84153483+Salypse@users.noreply.github.com>
2025-10-01 11:33:57 -0500

    Create winnerText that displays winner name at end of game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function newGameDisplay() {
     const gridContainer = document.querySelector("#game-container")
     const currentPlayerText = document.querySelector(".current-player")
+    const gameWinnerText = document.querySelector(".winner")
     const resetGameButton = document.querySelector(".reset")
 
     /* Player 1 is 0, Player2 is 1*/
@@ -32,22 +33,33 @@ function newGameDisplay() {
         if (currentPlayer === 0 && !item.textContent) {
             item.textContent = "X"
             item.style.color = "#2a9d8f";
-            gameController.checkForWin(gridItems)
-            /*Switch to player2 turn */
-            currentPlayerText.textContent = `${player2.name}'s Turn`
-            currentPlayer = 1;
+    
+            if (gameController.checkForWin(gridItems)) {
+                currentPlayerText.textContent = ""
+                gameWinnerText.textContent = `${player1.name} Wins!`
+            } else {
+                /*Switch to player2 turn */
+                currentPlayerText.textContent = `${player2.name}'s Turn`
+                currentPlayer = 1;
+            }
         }
         else if (currentPlayer === 1 && !item.textContent) {
             item.textContent = "O"
             item.style.color = "#ecd717";
-            gameController.checkForWin(gridItems)
-            /*Switch to player1 turn */
-            currentPlayerText.textContent = `${player1.name}'s Turn`
-            currentPlayer = 0;
+
+            if (gameController.checkForWin(gridItems)) {
+                currentPlayerText.textContent = ""
+                gameWinnerText.textContent = `${player2.name} Wins!`
+            } else {
+                /*Switch to player1 turn */
+                currentPlayerText.textContent = `${player1.name}'s Turn`
+                currentPlayer = 0;
+            }
         }
     }
 
     function setDefaultText() {
+        gameWinnerText.textContent = ""
         if (currentPlayer === 0) {
             return `${player1.name}'s Turn`
         } else if (currentPlayer === 1) {
@@ -64,7 +76,7 @@ function newGameDisplay() {
         currentPlayerText.textContent = setDefaultText()
     })
 
-    const currentGame = newGrid()
+    let currentGame = newGrid()
     const gameController = newGameController()
 
     return {newGrid}
